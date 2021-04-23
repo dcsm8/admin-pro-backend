15f0251d3ff7e4d8f7b7312ec6115c8ea53f1688
David Sánchez <deividsanchez96@hotmail.com>
2021-04-23 09:52:43 -0500

    Fin seccion 13

--- index.js
+++ index.js
@@ -17,10 +17,17 @@ app.use( express.json() );
 // Base de datos
 dbConnection();
 
+// Directorio público
+app.use( express.static('public') );
+
 
 // Rutas
 app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/hospitales', require('./routes/hospitales') );
+app.use( '/api/medicos', require('./routes/medicos') );
+app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
+app.use( '/api/upload', require('./routes/uploads') );
 
 
 
